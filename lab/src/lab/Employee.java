4ef80b97949573ca10646829620681205483003f
lagarwal <lagarwal@din51002532.corp.capgemini.com>
2018-04-16 13:14:45 +0530

    emp11--

--- lab/src/lab/Employee.java
+++ lab/src/lab/Employee.java
@@ -22,4 +22,8 @@ public class Employee {
 		this.address = address;
 		}
 
+	
+ 		
+    	 
+     
 }
--- lab/src/lab/entry.java
+++ lab/src/lab/entry.java
@@ -1,32 +1,24 @@
 package lab;
 
 public class entry {
-	 Employee e;
-     public void displayDetails()
-     {
-    	
-    	 System.out.print("\tName: "+ e.getName() );
- 		System.out.println("\tAddress: "+ e.getAddress().getLine());
- 		System.out.println("Country: "+ e.getAddress().getCountry().getName());
- 		//System.out.println("\tjoinmonth: "+ d.getDay() );
- 		System.out.println("City: " +  e.getAddress().getCountry().getCity().getName());
- 		//System.out.print("\tjoinmonth: "+ d.getMonth());
- 		//System.out.print("\tjoinmonth: "+ d.getYear());
- 		System.out.println("--------------------------------------------");
- 		//System.out.print("\tdisplay id"+ id);
-    	 
-     }
+	 
      public static void main(String[] args){
 	
 		Employee e = null;
 		City c = new City ("Jaipur");
 		Country co = new Country("INDIA",c);
-		
+		//co.setCity(c)
 	    Address ad = new Address("23",co);
-	    
+	    //ad.setCountry(co);
 	    e = new Employee("Laveena",ad);
+	    //e.displayDetails();
 	    
-		
+	    System.out.print("\tName: "+ e.getName() );
+ 		System.out.println("\tAddress: "+ e.getAddress().getLine());
+ 		System.out.println("Country: "+ e.getAddress().getCountry().getName());
+ 		
+ 		System.out.println("City: " + e.getAddress().getCountry().getCity().getName());
+ 		System.out.println("-----------------");
 	}
 }
 
